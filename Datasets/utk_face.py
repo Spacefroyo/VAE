a7c5007b058a4166415162f3f6b3ecd8fb1dec7e
Spacefroyo <pygao@ucsd.edu>
2024-03-23 15:46:09 -0700

    trained autoencoder demonstration

--- Datasets/utk_face.py
+++ Datasets/utk_face.py
@@ -1,14 +1,15 @@
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision.transforms import transforms
+from torch import Tensor
 import glob
 import numpy as np
 
 class UTKFaceDataset(Dataset):
-    def __init__(self, dir="UTKFace", target_transform=None):
+    def __init__(self, dir="UTKFace"):
         self.dir = dir
         self.transform = (lambda x : x.float() / 255.)
-        self.target_transform = target_transform
+        self.target_transform = lambda arr : Tensor([int(x) for x in arr])
         self.paths = glob.glob(self.dir + "/*")
 
     def __len__(self):
@@ -23,7 +24,7 @@ class UTKFaceDataset(Dataset):
     def __getitem__(self, idx):
         img_path: str = self.paths[idx]
         image = read_image(img_path)
-        label = img_path.split("_")[:3]
+        label = img_path[len(self.dir)+1].split("_")[:3]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
