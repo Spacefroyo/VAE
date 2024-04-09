9c20e63bda7159b30e59c912a7ccb46e72cb6757
Spacefroyo <pygao@ucsd.edu>
2024-04-08 20:33:34 -0700

    fgan v1 (fail)

--- Datasets/utk_face.py
+++ Datasets/utk_face.py
@@ -2,14 +2,18 @@ from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision.transforms import transforms
 from torch import Tensor
+import torch
 import glob
 import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
 
 class UTKFaceDataset(Dataset):
     def __init__(self, dir="UTKFace"):
         self.dir = dir
         self.transform = (lambda x : x.float() / 255.)
-        self.target_transform = lambda arr : Tensor([int(x) for x in arr])
+        mins, maxs = [0.0, 0.0, 0.0, 2017010997264384.0], [116.0, 1.0, 4.0, 2.0170116468781875e+17]
+        self.target_transform = lambda arr : torch.tensor([(float(arr[i]) - mins[i]) / maxs[i] for i in range(len(arr))])
         self.paths = glob.glob(self.dir + "/*")
 
     def __len__(self):
@@ -24,9 +28,33 @@ class UTKFaceDataset(Dataset):
     def __getitem__(self, idx):
         img_path: str = self.paths[idx]
         image = read_image(img_path)
-        label = img_path[len(self.dir)+1].split("_")[:3]
+
+        prefix = "UTKFace/"
+        postfix = ".jpg.chip.jpg"
+        label = img_path[len(prefix):-len(postfix)].split("_")
+        if (len(label) != 4):
+            label.insert(1, 0.5) # uncertain gender
+        
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
+    
+dataset = UTKFaceDataset()
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1,
+                                        shuffle=True, num_workers=0)
+data_iter = iter(dataloader)
+
+mins, maxs = [np.inf] * 4, [0] * 4
+for _ in range(len(dataset)):
+    images, label = next(data_iter)
+    if (images == None): break
+    label = label[0]
+    if label.shape[0] != 4:
+        print(label)
+    for i in range(len(mins)):
+        mins[i] = min(mins[i], label[i].item())
+        maxs[i] = max(maxs[i], label[i].item())
+
+print(mins, maxs)
